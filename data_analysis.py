c7004327993f2dc4fe7e88cd417c754c64377a94
silverleaf321 <silverleaf.yen@gmail.com>
2023-07-14 02:25:52 -0700

    add education level vs salary graph

--- data_analysis.py
+++ data_analysis.py
@@ -6,13 +6,18 @@ path = "C:/Users/User/Silverleaf/Code'23/BCA-project/Salary_Data.csv"
 
 df = pd.read_csv(path)
 
-# scatterplot
+# scatterplot: age vs salary
 sns.scatterplot(data = df, x = df["Age"], y = df["Salary"])
 plt.xlabel("Age")
 plt.ylabel("Salary")
 plt.title("Salary vs Age Scatterplot")
-plt.savefig("../plots/Salary_vs_Age_Scatterplot.png")
+plt.savefig("Salary_vs_Age_Scatterplot.png")
 
-
+plt.figure(figsize=(10, 6))
+sns.scatterplot(data = df, x = df["Education Level"], y = df["Salary"])
+plt.xlabel("Education Level")
+plt.ylabel("Salary")
+plt.title("Salary vs Education Level Scatterplot")
+plt.savefig("Education_Level_vs_Salary_Scatterplot.png")
 
 
