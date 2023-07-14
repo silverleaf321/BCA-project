ca02ac848509e9378462118378c87a26e8b6e464
silverleaf321 <silverleaf.yen@gmail.com>
2023-07-14 10:42:02 -0700

    read csv in cleanup file then export to analysis file

--- data_cleanup.py
+++ data_cleanup.py
@@ -1,11 +1,12 @@
-import data_analysis
+
 import pandas as pd
 
-df = data_analysis.df
+path = "C:/Users/User/Silverleaf/Code'23/BCA-project/Salary_Data.csv"
+df = pd.read_csv(path)
 
 for column in df.columns:
     df[column].fillna(df[column].mode().iloc[0], inplace=True)
 
 null_values = df.isnull().sum()
-print("Columns with null values:")
-print(null_values[null_values > 0])
+# print("Columns with null values:")
+# print(null_values[null_values > 0])
