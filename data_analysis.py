5930ab83d1dcf6e0d6cf88ed831c98cd3ced29ed
silverleaf321 <silverleaf.yen@gmail.com>
2023-07-13 23:49:21 -0700

    salaray vs age scatterplot code

---
+++ data_analysis.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+path = "C:/Users/User/Silverleaf/Code'23/BCA-project/Salary_Data.csv"
+
+df = pd.read_csv(path)
+
+# scatterplot
+sns.scatterplot(data = df, x = df["Age"], y = df["Salary"])
+plt.xlabel("Age")
+plt.ylabel("Salary")
+plt.title("Salary vs Age Scatterplot")
+plt.savefig("Salary_vs_Age_Scatterplot.png")
+
+
+
+
