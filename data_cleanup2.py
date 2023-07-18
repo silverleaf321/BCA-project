3e751263976db8029c1600a50dadabf5ff1b2370
silverleaf321 <silverleaf.yen@gmail.com>
2023-07-18 02:05:16 -0700

    added code for salary_data_with_dummies

--- data_cleanup2.py
+++ data_cleanup2.py
@@ -1,1 +1,8 @@
 import pandas as pd
+
+df = pd.read_csv("Salary_Data.csv")
+
+dummies = pd.get_dummies(df["Education Level"])
+data_with_dummies = pd.concat([df, dummies], axis = 1)
+
+data_with_dummies.to_csv("Salary_Data_with_dummies.csv", index = False)
