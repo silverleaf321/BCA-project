9b67ce5247418774e6e6c9e087d9b2faae4dab98
silverleaf321 <silverleaf.yen@gmail.com>
2023-07-18 10:37:16 -0700

    turn gender into boolean

--- data_cleanup_with_dummies.py
+++ data_cleanup_with_dummies.py
@@ -4,5 +4,7 @@ df = pd.read_csv("Salary_Data.csv")
 
 dummies = pd.get_dummies(df["Education Level"])
 data_with_dummies = pd.concat([df, dummies], axis = 1)
+dummies = pd.get_dummies(df["Gender"])
+data_with_dummies = pd.concat([data_with_dummies, dummies], axis = 1)
 
 data_with_dummies.to_csv("Salary_Data_with_dummies.csv", index = False)
