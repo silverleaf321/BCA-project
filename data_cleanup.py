6ce4437da689c66a3ab525aa02ef599bb2663d39
silverleaf321 <silverleaf.yen@gmail.com>
2023-07-20 12:16:07 -0700

    clean up

--- data_cleanup.py
+++ data_cleanup.py
@@ -1,18 +1,11 @@
 import pandas as pd
 
-path = "C:/Users/User/Silverleaf/Code'23/BCA-project/Salary_Data.csv"
+path = "Salary_Data.csv"
 df = pd.read_csv(path)
-
-# # before
-# null_values = df.isnull().sum()
-# print("Columns with null values:")
-# print(null_values[null_values > 0])
-# print("*********************")
 
 for column in df.columns:
     df[column].fillna(df[column].mode().iloc[0], inplace=True)
 
-# after
 null_values = df.isnull().sum()
 print("Columns with null values:")
 print(null_values[null_values > 0])
