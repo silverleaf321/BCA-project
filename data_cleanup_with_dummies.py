8056a98aa5b122e00d9f410c92a20986dffc3ed6
silverleaf321 <silverleaf.yen@gmail.com>
2023-07-20 12:01:54 -0700

    stuff woks!

--- data_cleanup_with_dummies.py
+++ data_cleanup_with_dummies.py
@@ -10,6 +10,8 @@ data_with_dummies = pd.concat([data_with_dummies, dummies], axis = 1)
 data_with_dummies.to_csv("Salary_Data_with_dummies.csv", index = False)
 
 df2 = pd.read_csv("Salary_Data_with_dummies.csv")
+bools = ["Bachelor", "Master", "High_School", "PhD", "Male", "Female", "Other"]
+df[bools] = df[bools].applymap(int)
 drop = ["Education Level", "Gender", "Job Title"]
 data_with_dummies2 = df2.drop(drop, axis = 1)
 data_with_dummies2.to_csv("Salary_Data_with_dummies.csv", index = False)
