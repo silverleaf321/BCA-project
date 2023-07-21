1611dd23dfba7c14be645192356ce8d8b1c95411
silverleaf321 <silverleaf.yen@gmail.com>
2023-07-20 23:33:08 -0700

    change image destinations

--- data_analysis.py
+++ data_analysis.py
@@ -2,9 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import seaborn as sns
-import data_cleanup
 
-df = data_cleanup.df
+path = "Salary_Data.csv"
+df = pd.read_csv(path)
 
 sns.set_style("darkgrid")
 sns.set_palette("pastel")
@@ -20,7 +20,7 @@ sm = cm.ScalarMappable(cmap="viridis_r")
 sm.set_array(df["Years of Experience"])
 cbar = plt.colorbar(sm)
 cbar.set_label("Years of Experience")
-# plt.savefig("Age_vs_Salary_Scatterplot.png")
+# plt.savefig("graphs\Age_vs_Salary_Scatterplot.png")
 
 # barplot: education level vs salary
 plt.figure(figsize=(10, 6))
@@ -28,7 +28,7 @@ sns.barplot(data = df, x = df["Education Level"], y = df["Salary"])
 plt.xlabel("Education Level")
 plt.ylabel("Salary")
 plt.title("Education Level vs Salary Barplot")
-# plt.savefig("Education_Level_vs_Salary_Barplot.png")
+# plt.savefig("graphs\Education_Level_vs_Salary_Scatterplot.png")
 
 # boxplot: salaries
 plt.figure(figsize=(10, 6))
@@ -36,7 +36,7 @@ sns.boxplot(data = df, x = df["Salary"])
 plt.title("Salary Boxplot")
 plt.xlabel("Salary")
 plt.ylabel("Values")
-# plt.savefig("Salary_Boxplot.png")
+# plt.savefig("graphs\Salary_Boxplot.png")
 
 # barplot: gender vs salary
 plt.figure(figsize=(10, 6))
@@ -44,7 +44,7 @@ sns.barplot(data = df, x = df["Gender"], y = df["Salary"])
 plt.xlabel("Gender")
 plt.ylabel("Salary")
 plt.title("Gender vs Salary Barplot")
-# plt.savefig("Gender_vs_Salary_Barplot.png")  
+# plt.savefig("graphs\Gender_vs_Salary_Barplot.png")  
 
 # sibe by side boxplot: gender vs salary
 plt.figure(figsize=(10, 6))
@@ -52,7 +52,7 @@ combined_df = [df["Gender"], df["Salary"]]
 labels = ["Gender", "Salary"]
 df = pd.DataFrame(dict(zip(labels, combined_df)))
 sns.boxplot(data = df, y = "Gender", x = "Salary")
-# plt.savefig("Gender_vs_Salary_Boxplot.png")
+# plt.savefig("graphs\Gender_vs_Salary_Boxplot.png")
 
 # stacked bar graph: education level vs gender
 # weirdest error ever
@@ -60,4 +60,4 @@ plt.figure(figsize = (10, 6))
 sns.countplot(data = df, x = df["Education Level"], hue = df["Gender"])
 plt.xlabel("Education Level")
 plt.ylabel("Count")
-# plt.savefig("Education_Level_vs_Gender_Countplot.png")
+plt.savefig("graphs\Education_Level_vs_Gender_Countplot.png")
--- data_cleanup.py
+++ data_cleanup.py
@@ -11,3 +11,9 @@ total_null_values = null_counts.sum()
 print("Total null values:")
 print(total_null_values)
 df.to_csv(path)
+
+path2 = "Salary_Data.csv"
+df2 = pd.read_csv("Salary_Data.csv")
+modes = df.mode().iloc[0]
+df2.fillna(modes, inplace=True)
+df2.to_csv(path2)
