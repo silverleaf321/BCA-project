65c4202357505b0f8d79ab05e662628d6907fde5
silverleaf321 <silverleaf.yen@gmail.com>
2023-07-20 20:33:07 -0700

    move salary column

--- data_cleanup_with_dummies.py
+++ data_cleanup_with_dummies.py
@@ -7,11 +7,17 @@ data_with_dummies = pd.concat([df, dummies], axis = 1)
 dummies = pd.get_dummies(df["Gender"])
 data_with_dummies = pd.concat([data_with_dummies, dummies], axis = 1)
 
-data_with_dummies.to_csv("Salary_Data_with_dummies.csv", index = False)
+data_with_dummies.to_csv("Salary_Data_cleaned.csv", index = False)
 
-df2 = pd.read_csv("Salary_Data_with_dummies.csv")
+df2 = pd.read_csv("Salary_Data_cleaned.csv")
 bool_cols = ["Bachelor", "Master", "High_School", "PhD", "Male", "Female", "Other"]
 df2[bool_cols] = df2[bool_cols].applymap(int)
 drop = ["Education Level", "Gender", "Job Title"]
 data_with_dummies2 = df2.drop(drop, axis = 1)
-data_with_dummies2.to_csv("Salary_Data_with_dummies.csv", index = False)
+data_with_dummies2.to_csv("Salary_Data_cleaned.csv", index = False)
+
+df3 = pd.read_csv("Salary_Data_cleaned.csv")
+third = df3.iloc[:, 2]
+df3.drop(df3.columns[2], axis = 1, inplace = True)
+df3["Salary"] = third
+df3.to_csv("Salary_Data_cleaned.csv", index = False)
